4119bcb8ea71e413ba0e5183d605effb2f9736d8
thanapat <69030100kmitl.ac.th>
2026-08-04 20:12:23 +0700

    add user input and greet function

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ def say_hello(name):
     print(f"Hello, {name}!")
 if __name__ == "__main__":
     say_hello("World")
+def say_hello(name):
+    print(f"Hello, {name}!")
+def greet_user():
+    name = input("Please enter your name: ")
+    say_hello(name)
+if __name__ == "__main__":
+    greet_user()
+
